c54ea40030eff271b3ce480f55aecfb6eb4a7baa
Kirti Singh <kirtisingh1497@gmail.com>
2022-09-21 13:42:54 +0530

    fix: handled api failure with catch

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ var input = document.querySelector(".input");
 var button = document.querySelector(".translate-button");
 var output = document.querySelector(".output");
 
-var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.jso";
 
 function constructUrl(text) {
   return serverUrl + "?" + "text=" + text;
-}
 
+}
+function errorHandler(error){
+  console.log("error"+ error);
+  alert("Something went wrong! Try after sometime");
+}
 function clickHandler() {
   console.log("Inside Click Handler")
   var userInput = input.value;
   fetch(constructUrl(userInput))
     .then((response) => response.json())
     .then((data) => {
-      if (data.success) {
         output.classList.remove('output-error');
         var outputResponse = data.contents.translated;
         output.innerText = outputResponse;
       }
-      if(data.error) {
-        var outputResponse = data.error.message;
-        output.innerText = outputResponse;
-        output.classList.add('output-error');
-      }
-    })
+      // if(data.error) {
+      //   var outputResponse = data.error.message;
+      //   output.innerText = outputResponse;
+      //   output.classList.add('output-error');
+      // }
+    )
+    .catch(errorHandler);
 }
 console.log('before click handler')
 button.addEventListener('click', clickHandler);
